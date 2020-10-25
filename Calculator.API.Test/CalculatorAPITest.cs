3f22070d1a1d270400c91eb3598bef0cd9ee1f52
Nitish Bhat <37233540+bhat-nitish@users.noreply.github.com>
2020-10-25 17:46:10 +0530

    Update Calculator.API unit tests

--- Calculator.API.Test/CalculatorAPITest.cs
+++ Calculator.API.Test/CalculatorAPITest.cs
@@ -41,21 +41,81 @@ namespace Calculator.API.Test
         }
 
         [Test]
-        public void MultiplyStrategy_Instantiated_Correctly()
+        public async Task MultiplyStrategy_Success_Handled()
         {
-
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(6, true, 200, new List<string>());
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ReturnsAsync(6);
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
         }
 
         [Test]
-        public void DivideStrategy_Instantiated_Correctly()
+        public async Task MultiplyStrategy_Failure_Handled()
         {
-
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(double.NaN, false, 500, new List<string>() { Resources.GenericException });
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ThrowsAsync(new Exception(Resources.GenericException));
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
         }
 
         [Test]
-        public void SubtractStrategy_Instantiated_Correctly()
+        public async Task DivideStrategy_Success_Handled()
         {
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(0.16666, true, 200, new List<string>());
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ReturnsAsync(0.16666);
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
+        }
 
+        [Test]
+        public async Task DivideStrategy_Failure_Handled()
+        {
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(double.NaN, false, 500, new List<string>() { Resources.GenericException });
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ThrowsAsync(new Exception(Resources.GenericException));
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
+        }
+
+        [Test]
+        public async Task SubtractStrategy_Success_Handled()
+        {
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(-4, true, 200, new List<string>());
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ReturnsAsync(-4);
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
+        }
+
+        [Test]
+        public async Task SubtractStrategy_Failure_Handled()
+        {
+            List<double> inputs = new List<double> { 1, 2, 3 };
+            CalculatorResponse expectedResponse = new CalculatorResponse();
+            expectedResponse.SetResult(double.NaN, false, 500, new List<string>() { Resources.GenericException });
+            var computeResolver = new Mock<IComputeResolver>();
+            computeResolver.Setup(cr => cr.Compute(inputs)).ThrowsAsync(new Exception(Resources.GenericException));
+            ICalculatorService calculator = new CalculatorService(computeResolver.Object);
+            var result = await calculator.Add(inputs);
+            expectedResponse.Should().BeEquivalentTo(result);
         }
     }
 }
